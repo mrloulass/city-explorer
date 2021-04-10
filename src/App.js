2ce21dd7e1c6337e090fba96ce7c911fabef6089
mrloulass <louislassegue@gmail.com>
2021-04-09 20:20:26 -0700

    reworked setup

--- src/App.js
+++ src/App.js
@@ -1,42 +1,17 @@
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
-import CityForm from './CityForm.js';
 import './App.css';
+// import Form from 'react-bootstrap/Form';
+// import Button from 'react-bootstrap/Button';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      awaitingToSearch: false,
-      searchingForCity: '',
-    };
-  }
-
-  searchResults = () => {
-    this.setState({ awaitingToSearch: false });
-  }
-
-  searchHandle = async(searchingForCity) => {
-    this.setState({
-      awaitingToSearch: true,
-      searchingForCity: searchingForCity
-    });
-
-    let cityLocationData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_KEY}&q=${searchingForCity}&format=json`);
-    console.log(cityLocationData);
-  }
-
-
-
+  
   render() {
     return (
       <div>
         <h1>City Explorer</h1>
-        <CityForm>
-          searchHandle={this.searchHandle}
-        </CityForm>
+        
       </div>
     );
   }
